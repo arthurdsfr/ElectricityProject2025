b32071d450d14310cf2257054d69111254859ef9
zetta-arthur-desfrancais <arthur.desfrancais@zetta-intern.space>
2025-12-06 22:45:49 +0100

    gru class

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#test 
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import scipy
+import matplotlib.pyplot as plt
+
+print("NumPy OK:", np.__version__)
+print("SciPy OK:", scipy.__version__)
+print("sklearn OK")
+print("Matplotlib OK")
